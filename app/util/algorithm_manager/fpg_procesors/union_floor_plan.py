dc12591995854100216c4bf04a54c70e7c4c1915
Mithila001 <136560946+Mithila001@users.noreply.github.com>
2026-05-01 09:50:59 +0530

    feat: Add union floor plan processor and documentation for algorithm manager

---
+++ app/util/algorithm_manager/fpg_procesors/union_floor_plan.py
@@ -0,0 +1,7 @@
+from app.algorithms.types.openings import FloorPlanWithOpenings
+
+
+def union_floor_plan(floor_plan: FloorPlanWithOpenings):
+    # Placeholder for the union floor plan processor function
+    # Return the combination of object that contain FloorPlanWithOpenings + unioned floor plan data (Wall data)
+    pass
